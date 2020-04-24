019722d637dc261e862933ae2f582cc10909bb0e
jeelbhanvadiya <jeelbhanvadiya781998@gmail.com>
2020-04-24 20:39:51 +0530

    quiz setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter, Route, Switch} from "react-router-dom"
+import './index.css';
 import './App.css';
+import Quiz from "./components/Quiz/index";
+import Home from "./components/Home";
+import Certificate from "./components/Certificate/index"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+  render() {
+    return (
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" name="Home" component={Home}/>
+              <Route  exact path="/quiz" name="Home" component={Quiz}/>
+              <Route  exact path="/certificate" name="Home" component={Certificate}/>
+            </Switch>
+          </BrowserRouter>
+
+    );
+  }
 }
 
 export default App;
+
+
